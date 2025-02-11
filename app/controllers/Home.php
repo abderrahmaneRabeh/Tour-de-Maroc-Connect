d0e4cbe243f2e4c0e66731c6048a674819448aa0
BensaltanaHoussam <bensaltna07@gmail.com>
2025-02-11 13:02:01 +0100

    creating navbar section

--- app/controllers/Home.php
+++
@@ -1,7 +0,0 @@
-<?php
-    use App\Lib\Controller;
-    class Home extends Controller {
-        public function index(){
-            $this->view("home");
-        }
-    }
--- app/views/pages.php
+++ app/views/pages.php
@@ -1,2 +1,11 @@
-mxckh
-    
+<body class="bg-blue-500">
+	<?php 
+	require "../app/inc/navbar.php" ;
+
+	?>
+
+
+
+</body>
+
+
