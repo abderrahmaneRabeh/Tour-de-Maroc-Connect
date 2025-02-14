7e945bc459804264b6b119e963a2fd591ca5e152
Abderrahmane Rabeh <rabehlife144@gmail.com>
2025-02-14 09:21:30 +0100

    Create Comment Class

---
+++ app/Classes/Comment.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Classes;
+
+class Comment
+{
+
+    private $id;
+    private $content;
+    private $fan_id;
+    private $post_id;
+    private $date_creation;
+
+    public function __construct($id = null, $content = null, $fan_id = null, $post_id = null, $date_creation = null)
+    {
+        $this->id = $id;
+        $this->content = $content;
+        $this->fan_id = $fan_id;
+        $this->post_id = $post_id;
+        $this->date_creation = $date_creation;
+    }
+
+    public function __get($attr)
+    {
+        return $this->$attr;
+    }
+
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+}
