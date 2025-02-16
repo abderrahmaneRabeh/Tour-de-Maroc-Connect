7bd3d5638f80ae228819d1d98bd745ce6adb69f4
asma828 <asmalachhab66@gmail.com>
2025-02-16 12:35:24 +0100

    add a helper method for flash messages

--- app/helpers/redirect.php
+++ app/helpers/redirect.php
@@ -3,3 +3,12 @@
     function redirect($page){
         header("location: ". URLROOT . "/" . $page);
     }
+
+    function flash($name, $message, $class = '') {
+        if(!empty($message)) {
+            $_SESSION[$name] = $message;
+            $_SESSION[$name.'_class'] = $class;
+        }
+    }
+    
+   
